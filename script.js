89c604efe6082543d2dd707d1f0135637fff7d4b
23Norm <114043352+23Norm@users.noreply.github.com>
2022-09-23 23:54:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+import Stack from './stack.js';
+
+let stack = new Stack
+
+let insertButton = document.getElementById('insert');
+let removeButton = document.getElementById('remove');
+let emptyButton = document.getElementById('empty');
+let input = document.getElementById('input');
+
+document.getElementById('refresh').addEventListener("click", () =>{
+    window.location = "/"
+})
+
+function makeNofication(notification){
+
+let message = document.getElementById("message")
+message.innerHTML = notification;
+
+setTimeout(() => {
+    message.innerHTML = ""
+}, 4000);
+}
+
+function addArray(){
+    let array = document.createElement("div");
+    array.className = 'array';
+
+document.getElementById('middle').appendChild(array)
+
+    for (let index = 0; index < stack.size; index++){
+
+      let box = document.createElement("div");
+      box.className = 'box';
+      box.innerHTML = stack.getStack()[index] == undefined ? "" : stack.getStack()[index];
+      array.appendChild(box)
+    }
+
+   array.children[stack.getCurrent].style.backgroundColor = "white"
+
+}
+
+insertButton.addEventListener ("click",() =>{
+    let insertValue = stack.insert(input.value)
+
+    if(insertValue == "Stack Overflow"){
+    makeNofication(insertValue)}
+    else{
+    addArray()}
+    
+})
+
+removeButton.addEventListener ("click",() =>{
+
+    let removeValue = stack.remove(input.value)
+
+    if(removeValue == "Stack Underflow"){
+    makeNofication(removeValue)}
+    else{
+    addArray()}
+
+})
+
+emptyButton.addEventListener ("click",() =>{
+    makeNofication(stack.empty())})
